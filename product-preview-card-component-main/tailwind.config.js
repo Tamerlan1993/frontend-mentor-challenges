6661126ee7f4f9e28030fc97ab42cbcc6d7bd3b7
Tamerlan.Pirmammadov <tamerlan.pirmammadov@guavapay.com>
2023-02-04 23:00:42 +0400

    update product preview card

---
+++ product-preview-card-component-main/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    fontFamily: {
+      Fraunces: ['Fraunces', 'serif'],
+      Montserrat: ['Montserrat', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        cream: "hsl(30, 38%, 92%)",
+        darkCyan: "hsl(158, 36%, 37%)",
+        veryDarkBlue: "hsl(212, 21%, 14%)",
+        darkGrayishBlue: "hsl(228, 12%, 48%)",
+      },
+    },
+  },
+  plugins: [],
+}
